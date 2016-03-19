e804373a0a60ace1afb83ea2881ee8600364e519
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-03-19 17:45:09 +0000

    SH08 Taking Mouse Input

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -11,28 +11,28 @@ public class BallSpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float velocity = 5.0f;
+        float speed = 5.0f;
         if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             GameObject instance = Instantiate(ballPrefab);
             Rigidbody rb = instance.GetComponent<Rigidbody>();
-            rb.velocity = Vector3.left * velocity;
+            rb.velocity = Vector3.left * speed;
         } else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             GameObject instance = Instantiate(ballPrefab);
             Rigidbody rb = instance.GetComponent<Rigidbody>();
-            rb.velocity = Vector3.right * velocity;
+            rb.velocity = Vector3.right * speed;
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             GameObject instance = Instantiate(ballPrefab);
             Rigidbody rb = instance.GetComponent<Rigidbody>();
-            rb.velocity = Vector3.forward * velocity;
+            rb.velocity = Vector3.forward * speed;
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             GameObject instance = Instantiate(ballPrefab);
             Rigidbody rb = instance.GetComponent<Rigidbody>();
-            rb.velocity = Vector3.back * velocity;
+            rb.velocity = Vector3.back * speed;
         }
     }
 }
--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -10,7 +10,10 @@ public class CameraRotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.rotation = Quaternion.Euler(0, 1, 0) * transform.rotation;
+        float rotationSpeed = 5.0f;
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+        transform.rotation = Quaternion.Euler(-mouseY, mouseX, 0) * transform.rotation;
 
     }
 }
