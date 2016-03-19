c6654ccb1a617f35677329c9edaa72c3e4760077
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-03-19 15:27:36 +0000

    SH06 Adding Velocity to the Ball

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+    public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float velocity = 5.0f;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.left * velocity;
+        } else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.right * velocity;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.forward * velocity;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.back * velocity;
+        }
+    }
+}
