5d7c866aa36954a26031e7683b04035050ff4ab3
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-03-21 14:38:48 +0000

    SH10 Controlling the Camera Gimbal

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -5,7 +5,6 @@ public class CameraRotation : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
@@ -13,7 +12,8 @@ public class CameraRotation : MonoBehaviour {
         float rotationSpeed = 5.0f;
         float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
         float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
-        transform.rotation = Quaternion.Euler(-mouseY, mouseX, 0) * transform.rotation;
-
+        transform.localRotation = Quaternion.Euler(0, mouseX, 0) * transform.localRotation;
+        Camera camera = GetComponentInChildren<Camera>();
+        camera.transform.localRotation = Quaternion.Euler(-mouseY, 0, 0) * camera.transform.localRotation;
     }
 }
