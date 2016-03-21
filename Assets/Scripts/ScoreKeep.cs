e6b0a54dcde95a393c8cd559920f0b47976d75dd
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-03-21 20:08:01 +0000

    SH13 Collisions in Code

---
+++ Assets/Scripts/ScoreKeep.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeep : MonoBehaviour {
+
+    int score = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        score++;
+        print("You scored. Your current score is " + score);
+    }
+}
